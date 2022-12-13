419188431a0f45ee5885444f66a37ff0f5569459
Savithri Wewala <s.w.wewala@gmail.com>
2022-12-13 17:54:32 +0900

    created adopt-me css index and app js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Honey",
+      animal: "Dog",
+      breed: "Golden Retriever",
+    }),
+    React.createElement(Pet, {
+      name: "Oden",
+      animal: "Possum",
+      breed: "Sugarglider",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
