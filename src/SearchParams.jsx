05f6b75c4f7b179da8651cb4c2a85e09fcb896f3
Savithri Wewala <s.w.wewala@gmail.com>
2023-01-10 15:31:47 +0900

    adding animala and breed to search

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,55 @@
+import { useState } from 'react';
+const ANIMALS = ["Bird", "Cat", "Dog", "Rabbit", "Reptile"];
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = []
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            onChange={e => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => {
+              setAnimal(e.target.value)
+              setBreed("")
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor='breed'>
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={e => {
+              setBreed(e.target.value)
+            }}
+          >
+            <option />
+            {breeds.map(breed => (
+              <option key={breed}></option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
