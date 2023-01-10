8e1cfb81a6436d4c766a2e61665fb41bb1277b78
Savithri Wewala <s.w.wewala@gmail.com>
2023-01-10 14:42:36 +0900

    refactored app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Honey",
-      animal: "Dog",
-      breed: "Golden Retriever",
-    }),
-    React.createElement(Pet, {
-      name: "Oden",
-      animal: "Sugarglider ",
-      breed: "Savanna Glider",
-    }),
-    React.createElement(Pet, {
-      name: "Candy",
-      animal: "Cat",
-      breed: "Mixed" }),
-  ]);
-};
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name = "Honey" animal = "Dog" breed = "Golden Retriever" />
+    <Pet name = "Oden" animal = "Sugarglider" breed = "Savanna Glider" />
+    <Pet name = "Candy" animal = "Cat" breed = "Mixed" />
+  </div>
+}
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
